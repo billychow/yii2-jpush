1b634f198650405f28f1b97c731d4d1e1fa78c6c
Billy <billy@dreammx.com>
2016-03-31 11:55:09 +0800

    initial commit

---
+++ JPush.php
@@ -0,0 +1,26 @@
+<?php
+namespace billychow\jpush;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+class Jpush extends Component
+{
+    public $appKey;
+    public $appSecret;
+    private $jPush;
+
+
+    public function init()
+    {
+        parent::init();
+        if(empty($this->appKey) || empty($this->appSecret)) {
+            throw new InvalidConfigException("appKey and appSecret cannot be empty!");
+        }
+        $this->jpush = new \jpush\jpush\JPush($this->appKey, $this->appSecret);
+    }
+
+    public function __call($method, $args = [])
+    {
+        return call_user_func_array(array($this->jPush, $method), $args);
+    }
+}
